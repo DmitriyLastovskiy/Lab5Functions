c92072401a569398c3fc4173d868406c14afee83
Dmytro Lastovskiy <lastovskiy555@gmail.com>
2023-01-04 14:35:15 +0300

    Добавьте файлы проекта.

---
+++ Lab5Functions/Lab5Functions.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+/*Функція яка рахує суму двох чисел
+* Реалізація або об'явлення
+int Sum(int a, int b)
+{
+	int result;
+	result = a + b;
+	return result;
+
+	return a + b;
+}
+ Виклик
+void main()
+{
+    int c = Sum(5,12);
+	cout<<c<<endl;
+	================
+	int q = 10;
+	int w = 10;
+
+	cout << Sum(q,w)<<endl;
+} */
+
+//Нюанс з передачею вказівника на массив а не сам массив
+void FillArray(int arr[], const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = i;
+	}
+}
+
+
+void ShowArray(int arr[], const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i]<<endl;
+	}
+}
+
+void main()
+{
+	const int SIZE = 10;
+	int arr[SIZE];
+
+
+	FillArray(arr, SIZE);
+	ShowArray(arr, SIZE);
+}
+
+
